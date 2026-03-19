fa9159e4c0a342ec3b54d3ab69db1316e0f0f639
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-19 17:00:37 -0400

    Add base bullet class and boss class

---
+++ Boss.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Numerics;
+
+public class Boss
+{
+	int Health = 100;
+	public static Vector2 Position = new Vector2(0, 0);
+	public void Attack()
+	{
+
+	}
+}
---
+++ Bullets.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+public class Bullets
+{
+    public static List<Vector2> Position = new List<Vector2>();
+    public void Move()
+    {
+
+    }
+}
