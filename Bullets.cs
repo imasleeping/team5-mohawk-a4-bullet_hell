bc148507a78e9cc25ceed728b2c6350f35d97b66
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-21 23:10:49 -0400

    Add bullet positions and velocitys

--- Bullets.cs
+++ Bullets.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using MohawkGame2D;
+using System;
 using System.Collections.Generic;
 using System.Numerics;
 
 public class Bullets
 {
-    public static List<Vector2> Position = new List<Vector2>();
-    public void Move()
+    public static List<Vector2> Positions = new List<Vector2>();
+    public static List<Vector2> Velocitys = new List<Vector2>();
+    public void MoveAndDraw()
     {
-
+        for (int i = 0; i < Positions.Count; i ++)
+        {
+            //move position by velocitys
+            Positions[i] += Velocitys[i] * Time.DeltaTime;
+            // draw circle graphics at positions
+            Draw.Circle(Positions[i], 50);
+        } 
     }
 }
