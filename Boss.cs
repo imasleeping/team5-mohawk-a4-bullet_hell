37281f463e30ff4d0d95b98bc181d6b800e4cfd5
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-22 00:19:17 -0400

    add boss drawing and bullet spawning/ pattern foudations

--- Boss.cs
+++ Boss.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Numerics;
-
-public class Boss
+namespace MohawkGame2D
 {
-	int Health = 100;
-	public static Vector2 Position = new Vector2(0, 0);
-	public void Attack()
+	public class Boss
 	{
+		int Health = 100;
+		public static Vector2 Position = Window.Size/2;
+		float AttackTimer = 0.5f;
+		int BulletAmount = 10;
+		Vector2 BulletSpawn = Window.Size / 2;
+        public void Attack()
+		{
+			AttackTimer -= Time.DeltaTime;
+			if (AttackTimer < 0)
+			{
 
+				float time = Time.SecondsElapsed; 
+				float cycle = time * MathF.Tau;
+				AttackTimer = 0.5f;
+					BulletSpawn.Y = Position.Y + 25;
+                    BulletSpawn.X = Position.X + 25;
+				//for (int i = 0; i < BulletAmount; i++)
+				//{
+				//	Console.WriteLine(BulletSpawn);
+                    Bullets.AddBullet(new Vector2(BulletSpawn.X, BulletSpawn.Y), BulletSpawn - Position);
+				//}
+            }
+			Draw.FillColor = Color.Red;
+			Draw.Circle(Position, 20);
+		}
 	}
 }
+
