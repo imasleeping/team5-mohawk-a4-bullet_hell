dd4c54dc49ca63399658245958fdeab0120757eb
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-21 23:13:34 -0400

    Add add bullet function

--- Bullets.cs
+++ Bullets.cs
@@ -7,6 +7,12 @@ public class Bullets
 {
     public static List<Vector2> Positions = new List<Vector2>();
     public static List<Vector2> Velocitys = new List<Vector2>();
+    public void AddBullet(Vector2 position, Vector2 velocity)
+    {
+        //adds new bullets position and velocity to their respective lists
+        Positions.Add(position);
+        Velocitys.Add(velocity);
+    }
     public void MoveAndDraw()
     {
         for (int i = 0; i < Positions.Count; i ++)
