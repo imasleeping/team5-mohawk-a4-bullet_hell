94dc00da0257e32a78fec256e81e19b58f5ce04d
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-23 09:51:39 -0400

    add a boss attack pattern

--- Boss.cs
+++ Boss.cs
@@ -6,9 +6,10 @@ namespace MohawkGame2D
 	{
 		int Health = 100;
 		public static Vector2 Position = Window.Size/2;
-		float AttackTimer = 0.5f;
+		float AttackTimer = 0.1f;
 		int BulletAmount = 10;
-		Vector2 BulletSpawn = Window.Size / 2;
+		int BulletSpeed = 5;
+        Vector2 BulletSpawn = Window.Size / 2;
         public void Attack()
 		{
 			AttackTimer -= Time.DeltaTime;
@@ -17,13 +18,13 @@ namespace MohawkGame2D
 
 				float time = Time.SecondsElapsed; 
 				float cycle = time * MathF.Tau;
-				AttackTimer = 0.5f;
-					BulletSpawn.Y = Position.Y + 25;
-                    BulletSpawn.X = Position.X + 25;
+				AttackTimer = 0.1f;
+					BulletSpawn.Y = Position.Y + MathF.Sin(cycle / 2) * 10;
+                    BulletSpawn.X = Position.X + MathF.Cos(cycle / 2) * 10;
 				//for (int i = 0; i < BulletAmount; i++)
 				//{
 				//	Console.WriteLine(BulletSpawn);
-                    Bullets.AddBullet(new Vector2(BulletSpawn.X, BulletSpawn.Y), BulletSpawn - Position);
+                    Bullets.AddBullet(new Vector2(BulletSpawn.X, BulletSpawn.Y), (BulletSpawn - Position) * BulletSpeed);
 				//}
             }
 			Draw.FillColor = Color.Red;
