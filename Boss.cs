c3a98fda8f2f4aefdcdf304a4243ae029fc2cbe3
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-23 10:10:55 -0400

    added easily changable boss pattern variables and switccable boss patterns

--- Boss.cs
+++ Boss.cs
@@ -7,26 +7,32 @@ namespace MohawkGame2D
 		int Health = 100;
 		public static Vector2 Position = Window.Size/2;
 		float AttackTimer = 0.1f;
+		float AttackTimerMax = 0.1f;
+		int AttackPattern = 1;
 		int BulletAmount = 10;
 		int BulletSpeed = 5;
         Vector2 BulletSpawn = Window.Size / 2;
         public void Attack()
 		{
-			AttackTimer -= Time.DeltaTime;
-			if (AttackTimer < 0)
+			if (AttackPattern == 1)
 			{
-
-				float time = Time.SecondsElapsed; 
-				float cycle = time * MathF.Tau;
-				AttackTimer = 0.1f;
+				AttackTimerMax = 0.1f;
+				BulletSpeed = 7;
+				AttackTimer -= Time.DeltaTime;
+				if (AttackTimer < 0)
+				{
+					float time = Time.SecondsElapsed; 
+					float cycle = time * MathF.Tau;
+					AttackTimer = AttackTimerMax;
 					BulletSpawn.Y = Position.Y + MathF.Sin(cycle / 2) * 10;
                     BulletSpawn.X = Position.X + MathF.Cos(cycle / 2) * 10;
-				//for (int i = 0; i < BulletAmount; i++)
-				//{
-				//	Console.WriteLine(BulletSpawn);
+					//for (int i = 0; i < BulletAmount; i++)
+					//{
                     Bullets.AddBullet(new Vector2(BulletSpawn.X, BulletSpawn.Y), (BulletSpawn - Position) * BulletSpeed);
-				//}
-            }
+					//}
+				}
+			}
+			// boss graphics
 			Draw.FillColor = Color.Red;
 			Draw.Circle(Position, 20);
 		}
