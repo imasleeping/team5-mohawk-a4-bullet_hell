cc64b7c85ff88e1f7db8b81be309d18f0b91973e
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-23 12:47:55 -0400

    Add attack pattern 2, change line color to red

--- Boss.cs
+++ Boss.cs
@@ -8,7 +8,7 @@ namespace MohawkGame2D
 		public static Vector2 Position = Window.Size/2;
 		float AttackTimer = 0.1f;
 		float AttackTimerMax = 0.1f;
-		int AttackPattern = 1;
+		int AttackPattern = 2;
 		int BulletAmount = 10;
 		int BulletSpeed = 5;
         Vector2 BulletSpawn = Window.Size / 2;
@@ -32,8 +32,26 @@ namespace MohawkGame2D
 					//}
 				}
 			}
-			// boss graphics
-			Draw.FillColor = Color.Red;
+            if (AttackPattern == 2)
+            {
+                AttackTimerMax = 5f;
+                BulletSpeed = 7;
+                AttackTimer -= Time.DeltaTime;
+				BulletAmount = 25;
+                if (AttackTimer < 0)
+                {
+                    AttackTimer = AttackTimerMax;
+                    for (int i = 0; i < BulletAmount; i++)
+                    {
+						BulletSpawn.Y = Position.Y + MathF.Sin(MathF.Tau * i / BulletAmount) * 10;
+						BulletSpawn.X = Position.X + MathF.Cos(MathF.Tau * i / BulletAmount) * 10;
+                        Bullets.AddBullet(new Vector2(BulletSpawn.X, BulletSpawn.Y), (BulletSpawn - Position) * BulletSpeed);
+                    }
+                }
+            }
+            // boss graphics
+            Draw.FillColor = Color.Red;
+			Draw.LineColor = Color.Red;
 			Draw.Circle(Position, 20);
 		}
 	}
