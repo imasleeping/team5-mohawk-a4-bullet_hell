abe1ae50bf05866759a18ed817aa4e985eff4715
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-24 11:10:48 -0400

    add attack pattern3

--- Boss.cs
+++ Boss.cs
@@ -10,8 +10,10 @@ namespace MohawkGame2D
 		float AttackTimerMax1 = 0.1f;
         float AttackTimer2 = 0.1f;
         float AttackTimerMax2 = 0.1f;
+        float AttackTimer3 = 0.1f;
+        float AttackTimerMax3 = 0.1f;
         float AttackPatternChange = 10f;
-        int AttackPattern = 1;
+        int AttackPattern = 3;
 		int BulletAmount = 10;
 		int BulletSpeed = 5;
         Vector2 BulletSpawn = Window.Size / 2;
@@ -21,12 +23,7 @@ namespace MohawkGame2D
 			if (AttackPatternChange <= 0)
 			{
                 AttackPatternChange = Random.Float(5f,15f);
-				AttackPattern += 1;
-				if (AttackPattern > 2)
-				{
-                    AttackPattern = 1;
-
-                }
+                AttackPattern = Random.Integer(1, 4);
             }
 
             if (AttackPattern == 1)
@@ -61,6 +58,24 @@ namespace MohawkGame2D
 						BulletSpawn.Y = Position.Y + MathF.Sin(MathF.Tau * i / BulletAmount) * 10;
 						BulletSpawn.X = Position.X + MathF.Cos(MathF.Tau * i / BulletAmount) * 10;
                         Bullets.AddBullet(new Vector2(BulletSpawn.X, BulletSpawn.Y), (BulletSpawn - Position) * BulletSpeed);
+                    }
+                }
+            }
+			if (AttackPattern == 3)
+			{
+                AttackTimerMax3 = 1.5f;
+                BulletSpeed = 5;
+                AttackTimer3 -= Time.DeltaTime;
+                BulletAmount = 20;
+                if (AttackTimer3 < 0)
+                {
+                    AttackTimer3 = AttackTimerMax3;
+                    Vector2 RandomPosition = new Vector2(Random.Float(0, Window.Size.Y), Random.Float(0, Window.Size.X));
+                    for (int i = 0; i < BulletAmount; i++)
+                    {
+                        BulletSpawn.Y = RandomPosition.Y + MathF.Sin(MathF.Tau * i / BulletAmount) * 10;
+                        BulletSpawn.X = RandomPosition.X + MathF.Cos(MathF.Tau * i / BulletAmount) * 10;
+                        Bullets.AddBullet(new Vector2(BulletSpawn.X, BulletSpawn.Y), (BulletSpawn - RandomPosition) * BulletSpeed);
                     }
                 }
             }
