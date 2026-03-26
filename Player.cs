5e761c564523a6b58edd809864b03b93d7b0ab0b
ColeM-Mohawk <000983139@mohawkcollege.ca>
2026-03-26 14:49:45 -0400

    Add setup player collision and movement

---
+++ Player.cs
@@ -0,0 +1,95 @@
+﻿// Include the namespaces (code libraries) you need below.
+using MohawkGame2D;
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Player
+    {
+        // Place your variables here:
+
+        Vector2 position;
+        Vector2 size;
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            // compute side of player
+            float playerLeftEdge = position.X;            // left edge
+            float playerRightEdge = position.X + size.X;  // right edge
+            float playerTopEdge = position.Y;             // top edge
+            float playerBottomEdge = position.Y + size.Y; // bottom edge
+
+            bool isLeftOfWindow = playerLeftEdge <= 0;             // left check
+            bool isRightOfWindow = playerRightEdge >= Window.Width;  // right check
+            bool isAboveWindow = playerTopEdge <= 0;             // top check
+            bool isBelowWindow = playerBottomEdge >= Window.Height; // bottom check
+
+            if (isLeftOfWindow)
+            {
+
+            }
+
+            if (isRightOfWindow)
+            {
+
+            }
+
+            if (isAboveWindow)
+            {
+
+            }
+
+            if (isBelowWindow)
+            {
+
+            }
+
+        }
+        public void GetPlayerInput()
+        {
+
+            Vector2 input = Vector2.Zero;
+
+            // UP
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Up) || Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                input.Y -= 1;
+            }
+
+            // DOWN
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Down) || Input.IsKeyboardKeyDown(KeyboardInput.S))
+            {
+                input.Y += 1;
+            }
+
+            // LEFT
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Left) || Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                input.X -= 1;
+            }
+
+            // RIGHT
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Right) || Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                input.X += 1;
+            }
+        }
+    }
+
+}
